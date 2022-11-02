14ef474f1717c15f69fcba889c7e79dba2542355
Ananda Prema <77527794+menandap@users.noreply.github.com>
2022-11-02 10:10:53 +0800

    up
    up

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserAuthController;
 
 // AUTH
-Route::view('/login', 'pages.login')->name('login'); 
+Route::view('/login', 'auth.login')->name('login'); 
 Route::post('/actLogin', [UserAuthController::class, 'actLogin']);
-Route::view('/register', 'pages.register')->name('register'); 
+Route::view('/register', 'auth.register')->name('register'); 
 Route::post('/actRegister', [UserAuthController::class, 'actRegister']);
 Route::post('/logout', [UserAuthController::class, 'logout'])->name('logout');
 
